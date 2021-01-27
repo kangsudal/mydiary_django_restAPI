04760332222fce468f1b57d74e65521b59ebf2df
kangsudal-ncloudserver <kangsudal@gmail.com>
2021-01-27 15:16:50 +0900

    view 생성

--- mydiaryApp/views.py
+++ mydiaryApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+'''REST_FRAMEWORK의 모듈을 이용하여 GET방식 API를 만드는 코드'''
+
+# from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Corpus
+from .serializers import CorpusSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hey kkakkung!");
